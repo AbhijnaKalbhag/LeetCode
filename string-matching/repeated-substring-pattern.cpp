96948ff772830ec2acd12e758d0f7eacddde9d5e
Abhijna <136738568+AbhijnaKalbhag@users.noreply.github.com>
2024-10-20 19:54:41 +0530

    LC-repeated-substring-pattern [Runtime: 4 ms; Memory: 14.3 MB]

--- string-matching/repeated-substring-pattern.cpp
+++ string-matching/repeated-substring-pattern.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        unordered_map<char,int>mp;
-        for(char c:s){
-            mp[c]++;
-        }
-        for(auto&[ch,cnt]:mp){
-            if(cnt%2!=0){
-                return false;
-            }
-        }
-        return true;
+       string ss=s+s;
+
+       return ss.substr(1,ss.length()-2).find(s)!=string::npos;
     }
 };
